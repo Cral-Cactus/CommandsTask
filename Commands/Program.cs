70d88c18040bbb0246726b549894637c4942e673
Cral_Cactus <marian.pb@abv.bg>
2023-03-09 10:18:45 +0200

    Upload

---
+++ Commands/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Commands
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            while (true)
+            {
+                string[] action = Console.ReadLine().ToLower().Split();
+
+                if (action[0] == "push")
+                {
+                    if (action.Length < 2)
+                    {
+                        Console.WriteLine("Error: Push command requires an argument");
+                        continue;
+                    }
+
+                    int element = int.Parse(action[1]);
+                    nums.Add(element);
+                }
+                else if (action[0] == "pop")
+                {
+                    int lastElement = nums.Last();
+                    Console.WriteLine(lastElement);
+
+                    nums.Remove(lastElement);
+                }
+                else if (action[0] == "shift")
+                {
+                    int lastElement = nums.Last();
+                    int firstElement = nums.First();
+
+                    nums.Remove(lastElement);
+                    nums.Insert(0, lastElement);
+                    nums.Add(firstElement);
+                }
+                else if (action[0] == "remove")
+                {
+                    if (action.Length < 2)
+                    {
+                        Console.WriteLine("Error: Remove command requires an argument");
+                        continue;
+                    }
+
+                    int removePos = int.Parse(action[1]);
+                    nums.RemoveAt(removePos);
+                }
+                else if (action[0] == "addMany")
+                {
+                    if (action.Length < 2)
+                    {
+                        Console.WriteLine("Error: addMany command requires an argument");
+                        continue;
+                    }
+
+                    int position = int.Parse(action[1]);
+
+                    List<int> elements = new List<int>();
+
+                    for (int i = 2; i < action.Length; i++)
+                    {
+                        elements.Add(int.Parse(action[i]));
+                    }
+
+                    nums.InsertRange(position, elements);
+                }
+                else if (action[0] == "print")
+                {
+                    nums.Reverse();
+
+                    Console.WriteLine(string.Join(", ", nums));
+                    break;
+                }
+            }
+        }
+    }
+}
